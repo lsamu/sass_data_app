7ac4140fe9f8b754083b4828d88549f0d894e387
lauxinyi <lauxinyi@gmail.com>
2022-11-04 14:39:21 +0800

    feat:master

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import Vue from 'vue';
-import VueCompositionAPI from '@vue/composition-api'
-Vue.use(VueCompositionAPI)
 
 import App from './App.vue';
 import router from './router'
@@ -10,16 +8,12 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI, { size: 'mini' });
 
-import BoxEditor from "./components/main";
-Vue.use(BoxEditor);
+import VCharts from "v-charts";
+Vue.use(VCharts);
 
 import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
 Vue.component('v-icon', Icon)
-
-import BoxUI from '../lib/box-ui/box-ui.min.js';
-import '../lib/box-ui/style.css'
-Vue.use(BoxUI)
 
 new Vue({
   router,
